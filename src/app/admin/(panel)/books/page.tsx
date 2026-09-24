d2c374cafc422965ef4bf29719143d5453af9ec9
RohitYadav8 <rohityadav40823@gmail.com>
2026-09-24 18:56:07 +0530

    Update books admin page

--- src/app/admin/(panel)/books/page.tsx
+++ src/app/admin/(panel)/books/page.tsx
@@ -1374,7 +1374,7 @@ export default function AdminBooksPage() {
                     </div>
                   </div>
 
-                  {/* FOOTER */}
+                  
 
                   <div
                     className="
